e9d253492fc756e4f8ba9b6ef4e6e22fef831dce
Naori <hemellycarolyne21@gmail.com>
2023-01-22 13:03:44 -0300

    changed colors

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
-const form = document.querySelector("form")
-const nlwSetup = new NLWSetup(form)
-const button = document.querySelector("header button")
+const form = document.querySelector("form");
+const nlwSetup = new NLWSetup(form);
+const button = document.querySelector("header button");
 
-button.addEventListener("click", add)
-form.addEventListener("change", save)
-
+button.addEventListener("click", add);
+form.addEventListener("change", save);
 
 function add() {
-  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
-  const dayExists = nlwSetup.dayExists(today)
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5);
+  const dayExists = nlwSetup.dayExists(today);
 
   if (dayExists) {
-    alert("Dia já incluso")
-    return
+    alert("Dia já incluso");
+    return;
   }
 
-  alert("Adicionado com sucesso ✅")
-  nlwSetup.addDay(today)
+  alert("Adicionado com sucesso ✅");
 
+  nlwSetup.addDay(today);
 }
 
 function save() {
-  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data));
 }
 
-const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {
+  run: ["01-21", "01-20", "01-19", "01-18", "01-17", "01-16", "01-15"]
+};
 
-nlwSetup.setData(data)
-nlwSetup.load()
-
-
+nlwSetup.setData(data);
+nlwSetup.load();
